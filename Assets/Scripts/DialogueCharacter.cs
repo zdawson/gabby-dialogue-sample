ce455f698b5a6cf11d5d2f2a6604a99e75ef2812
Zack Dawson <zdawson@users.noreply.github.com>
2020-07-15 21:06:43 -0400

    Dialogue sample with basic functionality

---
+++ Assets/Scripts/DialogueCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gabby Dialogue Sample/Character Definition")]
+public class DialogueCharacter : ScriptableObject
+{
+    [Serializable]
+    private class CharacterPortrait
+    {
+        public string name = "default";
+        public Sprite portrait = null;
+    }
+
+    public string internalName = "NewCharacter";
+    public string displayName = "New Character";
+    [SerializeField]
+    private CharacterPortrait[] _portraits = null;
+
+    [NonSerialized]
+    public Dictionary<string, Sprite> portraits = null;
+
+    public void InitPortraits()
+    {
+        portraits = new Dictionary<string, Sprite>();
+        foreach (CharacterPortrait portrait in _portraits)
+        {
+            portraits.Add(portrait.name, portrait.portrait);
+        }
+    }
+}
---
+++ Assets/Scripts/SampleDialogueSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using GabbyDialogue;
+
+namespace GabbyDialogueSample
+{
+    public class SampleDialogueSystem : MonoBehaviour
+    {
+        private static SampleDialogueSystem _instance = null;
+
+        private DialogueInterface dialogueUI;
+
+        private DialogueEngine dialogueEngine;
+        private Dictionary<string, DialogueCharacter> characters = new Dictionary<string, DialogueCharacter>();
+
+        public static SampleDialogueSystem instance()
+        {
+            if (!_instance)
+            {
+                _instance = new GameObject("_SampleDialogueSystem").AddComponent<SampleDialogueSystem>();
+            }
+            return _instance;
+        }
+
+        private void Awake()
+        {
+            dialogueUI = (Instantiate(Resources.Load("Prefabs/DialogueUI", typeof(GameObject))) as GameObject).GetComponentInChildren<DialogueInterface>();
+            dialogueUI.gameObject.name = "_SampleDialogueUI";
+            dialogueUI.gameObject.SetActive(false);
+
+            // Load character definitions
+            Object[] resources = Resources.LoadAll("Characters/");
+            foreach (Object resource in resources)
+            {
+                if (resource is DialogueCharacter)
+                {
+                    DialogueCharacter character = (DialogueCharacter)resource;
+                    character.InitPortraits();
+                    characters.Add(character.internalName, character);
+                }
+            }
+        }
+
+        public void PlayDialogue(Dialogue dialogue)
+        {
+            DialogueCharacter character = characters[dialogue.CharacterName];
+            dialogueUI.SetCharacter(character.displayName, character.portraits["default"]);
+            dialogueUI.SetDialogueText(dialogue.DialogueName);
+            dialogueUI.gameObject.SetActive(true);
+        }
+    }
+}
